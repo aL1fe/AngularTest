e3d6d053baba63f1407c8b08a3af80c00a4a403e
alexei.smirnov <alexei.smirnov@hys-enterprise.com>
2022-02-18 14:09:31 +0200

    add picture

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
 
   constructor() { }
 
   title: string = `My Form title`;
   text: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, esse.";
- 
- 
- 
-  ngOnInit(): void {
-  }
+  obj = {
+          name: `Bobby`, 
+          obj2: 
+          {
+            age: 25, 
+            job: `Welder` 
+          }
+        } 
 
+  //imgUrl: string = `https://img.icons8.com/stickers/100/000000/spa-flower.png`;
+  imgUrl: string = `/assets/images/icons-spa-100.png`;
+  
+  getInfo(): string
+  {
+    return `My info`
+  }
 }
